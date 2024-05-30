1e02ac6521af836752a64a639a6660f3ddd144af
designer-ab <per.useravi0@gmail.com>
2024-05-30 15:56:15 +0600

    added rating system and comment delete features

--- src/Pages/Details/Details.jsx
+++ src/Pages/Details/Details.jsx
@@ -4,11 +4,16 @@ import { useQuery } from "@tanstack/react-query";
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../Providers/AuthProviders";
 import Swal from "sweetalert2";
+import { Rating } from '@smastrom/react-rating'
+
+import '@smastrom/react-rating/style.css'
 
 const Details = () => {
   const { id } = useParams();
   const { user } = useContext(AuthContext);
+  const [rating, setRating] = useState(0);
   const [userComment, setUserComment] = useState([]);
+  console.log(rating);
 
   const { isLoading, data } = useQuery({
     queryKey: ["blogs data"],
@@ -34,6 +39,7 @@ const Details = () => {
       });
   },[id]);
 
+
   if (isLoading) {
     return <Spinner color="purple" aria-label="Purple spinner example" />;
   }
@@ -46,6 +52,9 @@ const Details = () => {
   const userEmail = user?.email;
 
   const showButton = dbEmail === userEmail;
+  
+
+  
   // console.log(dbEmail, userEmail);
 
   const handleSubmit = (e) => {
@@ -55,8 +64,10 @@ const Details = () => {
     const blogs_id = id;
     const user_name = user.displayName;
     const user_image = user.photoURL;
+    const user_email = user.email;
+    const rate = rating;
 
-    const Comment = { comment, blogs_id, user_name, user_image };
+    const Comment = { comment, blogs_id, user_name, user_image, rate, user_email };
     // console.log(userComment);
 
     fetch("https://blog-hero-server.vercel.app/comments", {
@@ -80,6 +91,29 @@ const Details = () => {
         }
         form.reset();
       });
+  };
+
+  const handleCommentDelete = (com) => {
+    fetch(`http://localhost:5000/comments/${com._id}`, {
+      method: "DELETE",
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+    .then(res => res.json())
+    .then(data => {
+      // console.log(data)
+      if (data.deletedCount) {
+        Swal.fire({
+          title: "Success!",
+          text: "Comment Deleted Successfully",
+          icon: "success",
+          confirmButtonText: "OK",
+        });
+        const remaining = userComment.filter((comment) => comment._id !== id);
+        setUserComment(remaining);
+      }
+    })
   };
 
   return (
@@ -111,7 +145,10 @@ const Details = () => {
             <h1 className="text-3xl font-semibold text-center">Comment Section</h1>
             <div className="flex flex-col md:flex-row gap-10 mt-5">
               <div className="flex-1">
+                <div>
                 <p className="text-xl font-semibold">All Comments : {userComment.length}</p>
+                <p>Rating : {userComment.reduce((acc, item) => acc + item.rate, 0)/ userComment.length}/5</p>
+                </div>
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-2">
                 {
                   userComment.map(com => <div className="border-2 border-[#4D869C] mb-2 p-2 flex gap-3 rounded-md" key={com._id}>
@@ -121,6 +158,9 @@ const Details = () => {
                     <div>
                       <h3><span className="font-medium">Name</span> : {com.user_name}</h3>
                       <p><span className="font-medium">Comment</span> : {com.comment}</p>
+                      {
+                        com.user_email === userEmail && <button onClick={() => handleCommentDelete(com)} className="py-2 mt-1 rounded px-4 border-red-400 bg-red-700 text-white">Delete</button>
+                      }
                     </div>
                   </div>)
                 }
@@ -133,6 +173,8 @@ const Details = () => {
                   </p>
                 ) : (
                   <div>
+                    <h1 className="text-xl font-semibold">Rate this blog</h1>
+                  <Rating style={{ maxWidth: 250 }} value={rating} onChange={setRating} />
                     <h3 className="text-xl font-semibold">Comments</h3>
                     <form onSubmit={handleSubmit} className="mt-2">
                       <textarea className="w-full border-2 border-[#4D869C] rounded-md" name="comment" id="comment" rows={8}></textarea>
